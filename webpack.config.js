602aca70251d8d66f89ebdc41fe6fa1ca2e1a11a
laibao <laibao@laibaodeMacBook-Pro.local>
2017-02-26 14:10:27 +0800

    first push

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports={
+	// entry:['./src/script/main.js','./src/script/a.js'],
+	entry:{
+		main:'./src/script/main.js',
+		a:'./src/script/a.js'
+	},
+	output:{
+		path:'./dist',
+		// filename:'[name]-[hash].js'
+		filename:'/js/[name]-[chunkhash].js'
+	},
+	plugins:[
+		new htmlWebpackPlugin({
+			template:'index.html'
+		})
+	]
+}
