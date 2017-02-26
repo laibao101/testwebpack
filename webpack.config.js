e7ffe5a37010a8678bb07beff03ffa2df17a4daf
laibao <laibao@laibaodeMacBook-Pro.local>
2017-02-26 16:07:56 +0800

    use html-webpack-plugin to create mulit pages

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ module.exports={
 	// entry:['./src/script/main.js','./src/script/a.js'],
 	entry:{
 		main:'./src/script/main.js',
-		a:'./src/script/a.js'
+		a:'./src/script/a.js',
+		b:'./src/script/b.js',
+		c:'./src/script/c.js'
 	},
 	output:{
 		path:'./dist',
@@ -15,15 +17,38 @@ module.exports={
 	plugins:[
 		new htmlWebpackPlugin({
 			template:'index.html',
-			filename:'index.html',
-			// inject:'head'
-			// inject:'body',
-			title:'webpack is good',
+			filename:'a.html',
+			title:'this is a.html',
 			date:new Date(),
-			inject:false,
+			inject:'body',
+			chunks:['main','a'],
 			minify:{
 				removeComments:true,
-				collapseWhitespace:true
+				collapseWhitespace:false
+			}
+		}),
+		new htmlWebpackPlugin({
+			template:'index.html',
+			filename:'b.html',
+			title:'this is b.html',
+			date:new Date(),
+			inject:'body',
+			chunks:['main','b'],
+			minify:{
+				removeComments:true,
+				collapseWhitespace:false
+			}
+		}),
+		new htmlWebpackPlugin({
+			template:'index.html',
+			filename:'c.html',
+			title:'this is c.html',
+			date:new Date(),
+			inject:'body',
+			chunks:['main','c'],
+			minify:{
+				removeComments:true,
+				collapseWhitespace:false
 			}
 		})
 	]
